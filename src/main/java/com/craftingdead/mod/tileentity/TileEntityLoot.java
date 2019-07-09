0956333b26d511b7627fcbd3a11ec305d1321bd2
Joseph Tarbit <joetarbit@gmail.com>
2019-07-10 00:47:59 +0100

    Change styling

--- src/main/java/com/craftingdead/mod/tileentity/TileEntityLoot.java
+++ src/main/java/com/craftingdead/mod/tileentity/TileEntityLoot.java
@@ -5,26 +5,26 @@ import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityLoot extends TileEntity implements ITickableTileEntity {
 
-	public TileEntityLoot() {
-		super(ModTileEntityTypes.LOOT);
-	}
+  public TileEntityLoot() {
+    super(ModTileEntityTypes.LOOT);
+  }
 
-	private static final int DESPAWN_TIME = 2400;
-	private static final int DESPAWN_DISTANCE = 50;
+  private static final int DESPAWN_TIME = 2400;
+  private static final int DESPAWN_DISTANCE = 50;
 
-	private int removalTick = 0;
+  private int removalTick = 0;
 
-	@Override
-	public void tick() {
-		if (this.world.getClosestPlayer(this.getPos().getX(), this.getPos().getY(), this.getPos().getZ(),
-				DESPAWN_DISTANCE, (entity) -> !entity.isSpectator()) == null) {
-			if (this.removalTick++ >= DESPAWN_TIME) {
-				if (!this.world.isRemote) {
-					this.world.destroyBlock(this.getPos(), false);
-					this.world.removeTileEntity(this.getPos());
-				}
-				this.removalTick = 0;
-			}
-		}
-	}
+  @Override
+  public void tick() {
+    if (this.world.getClosestPlayer(this.getPos().getX(), this.getPos().getY(),
+        this.getPos().getZ(), DESPAWN_DISTANCE, (entity) -> !entity.isSpectator()) == null) {
+      if (this.removalTick++ >= DESPAWN_TIME) {
+        if (!this.world.isRemote) {
+          this.world.destroyBlock(this.getPos(), false);
+          this.world.removeTileEntity(this.getPos());
+        }
+        this.removalTick = 0;
+      }
+    }
+  }
 }
