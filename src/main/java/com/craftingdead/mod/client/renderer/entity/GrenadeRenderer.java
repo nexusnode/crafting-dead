9d74429741bc8dd2c4545ae0579d43f4d042fe9d
Alice <kodekishin@gmail.com>
2020-02-13 04:17:03 +0300

    Tried to update models

--- src/main/java/com/craftingdead/mod/client/renderer/entity/GrenadeRenderer.java
+++ src/main/java/com/craftingdead/mod/client/renderer/entity/GrenadeRenderer.java
@@ -4,10 +4,12 @@ import com.craftingdead.mod.CraftingDead;
 import com.craftingdead.mod.client.renderer.entity.model.ModelGrenade;
 import com.craftingdead.mod.entity.GrenadeEntity;
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.ResourceLocation;
 
 public class GrenadeRenderer  extends EntityRenderer<GrenadeEntity>{
@@ -22,10 +24,15 @@ public class GrenadeRenderer  extends EntityRenderer<GrenadeEntity>{
 	}
 	
 	@Override
-	public void render(GrenadeEntity entity, float p_225623_2_, float p_225623_3_, MatrixStack p_225623_4_,
-			IRenderTypeBuffer p_225623_5_, int p_225623_6_) {
-		//model.render(p_225623_4_, p_225598_2_, p_225598_3_, p_225598_4_, p_225598_5_, p_225598_6_, p_225598_7_, p_225598_8_);
-		super.render(entity, p_225623_2_, p_225623_3_, p_225623_4_, p_225623_5_, p_225623_6_);
+	public void render(GrenadeEntity entity, float entityYaw, float partialTicks,
+		      MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int p_225623_6_) {
+	
+		matrixStack.scale(0.4f, 0.4f, 0.4f);
+		
+		IVertexBuilder vertexBuilder =
+		        renderTypeBuffer.getBuffer(model.getLayer(this.getEntityTexture(entity)));
+		model.render(matrixStack, vertexBuilder, p_225623_6_, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F,
+	            1.0F, 0.15F);
 	}
 
 	@Override
