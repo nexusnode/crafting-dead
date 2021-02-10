5aafb6c960612bbecf6de9ff2d551fc809941bf7
Joseph Tarbit <joetarbit@gmail.com>
2021-02-10 14:52:08 +0000

    Bump Forge & FG & Gradle & mod versions; update licenses

--- crafting-dead-core/src/main/java/com/craftingdead/core/client/gui/HitMarker.java
+++ crafting-dead-core/src/main/java/com/craftingdead/core/client/gui/HitMarker.java
@@ -1,6 +1,6 @@
-/**
+/*
  * Crafting Dead
- * Copyright (C) 2020  Nexus Node
+ * Copyright (C) 2021  NexusNode LTD
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.craftingdead.core.client.gui;
 
 import java.util.Optional;
